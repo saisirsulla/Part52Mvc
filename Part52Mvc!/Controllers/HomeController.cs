9be4425db7362ce3c8dcf3e07dfb0e9625358d13
saisirsulla <saisirsulla98211@gmail.com>
2020-01-13 16:22:07 +0530

    first commit

---
+++ Part52Mvc!/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Part52Mvc_.Models;
+namespace Part52Mvc_.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            SampleDBContext db = new SampleDBContext();
+            List<Employee> employee = db.Employees.ToList();
+            return View(employee);
+        }
+    }
+}
---
+++ Part52Mvc!/Models/EmployeeMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Part52Mvc_.Models
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+
+    }
+    public class EmployeeMetadata
+    {
+        [DisplayFormat(NullDisplayText = "Gender not specified")]
+        public string Gender { get; set; }
+    }
+}
